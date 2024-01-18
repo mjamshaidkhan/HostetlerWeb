1d916c3299e723e606b239ab002656db19a11bf2
Muhammad Jamshaid <muhammadjamshaidkhan@hotmail.com>
2024-01-18 23:04:36 +0500

    fix

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,6 +1,4 @@
 
-import chair from '../../public/chair/ch_1.jpg'
-
 export {  };
 
 export const data = {
@@ -9,7 +7,7 @@ export const data = {
   },
   furniture : [
     {
-      image:chair,
+      image:"",
       imagesBaseURL:"",
       colors:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
  
